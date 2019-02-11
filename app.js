e589e6731da4197445362322bf52f6695e1bf992
Daniel Boffey <dboffey70@googlemail.com>
2019-02-11 22:50:24 +0000

    Added responsive queries, created issue with mobile side bar(needs resolving)

--- app.js
+++ app.js
@@ -140,10 +140,10 @@ function init(){
 
 function openSlideMenu(){
   document.getElementById('side-menu').style.width = '250px';
-  document.getElementById('main').style.marginLeft = '250px';
+  // document.getElementById('main').style.marginLeft = '250px';
   }
 
   function closeSideMenu(){
     document.getElementById('side-menu').style.width = '0';
-    document.getElementById('main').style.marginLeft = '0';
+    // document.getElementById('main').style.marginLeft = '0';
     }
